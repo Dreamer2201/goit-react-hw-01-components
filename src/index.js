cd97c2fb0b85638ab4ebf918f7b8b5fc7e2571ab
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-09-09 09:02:46 +0300

    render profile#1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { createElement } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'));
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
