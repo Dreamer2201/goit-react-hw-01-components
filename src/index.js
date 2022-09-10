ed3d70c490cd300cdd8509f29402649a60486a4a
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-09-10 11:04:53 +0300

    remove import which not use

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { createElement } from 'react';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
